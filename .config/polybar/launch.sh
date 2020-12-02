50189fc8419071c44e04acdbb94a5f9f7ddef5bd
Kien Nguyen <kiennt65@viettel.com.vn>
2020-12-02 15:15:39 +0700

    Update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,6 +10,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 # polybar main -c ~/.config/polybar/config.ini &
-MONITOR=DP-1 polybar top -c ~/.config/polybar/configtop-left &
-MONITOR=DP-1 polybar top -c ~/.config/polybar/configtop-center &
-MONITOR=DP-1 polybar top -c ~/.config/polybar/configtop-right &
+#MONITOR=DP-1 polybar top -c ~/.config/polybar/configtop-left &
+#MONITOR=DP-1 polybar top -c ~/.config/polybar/configtop-center &
+#MONITOR=DP-1 polybar top -c ~/.config/polybar/configtop-right &
+MONITOR=DP-1 polybar top -c ~/.config/polybar/configtop &
